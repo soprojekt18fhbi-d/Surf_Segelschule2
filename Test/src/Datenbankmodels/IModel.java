332922647e83cb82d2245fee4ed2ae18c4b51882
benkroencke <benkroencke@web.de>
2018-12-01 21:03:22 +0100

    IModel

---
+++ Test/src/Datenbankmodels/IModel.java
@@ -0,0 +1,46 @@
+package Datenbankmodels;
+
+import java.util.ArrayList;
+
+import javax.swing.DefaultListModel;
+
+
+
+public interface IModel {
+
+	
+	/**
+	 * ArrayList für Möglichkeit der Aufnahme mehrerer Observer(Views)
+	 * 
+	 * 
+	 */
+	public ArrayList<IView> observers = new ArrayList<IView>();
+	
+	
+	
+	/**
+	 * Anmelden einer View.
+	 * 
+	 *
+	 */
+	public void anmelden(IView view);
+
+	/**
+	 * Abmelden einer View.
+	 * 
+	 * 
+	 */
+	public void abmelden(IView view);
+	
+	
+	/**
+	 * Durchgehen der Observer und Updateaufruf.
+	 * 
+	 *
+	 */
+	public void updateObserver();
+	
+	public DefaultListModel getKunden();
+	
+	
+}
