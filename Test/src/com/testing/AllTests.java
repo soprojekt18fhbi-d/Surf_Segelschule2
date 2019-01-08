0976e1a6b357aab37e5c5c18972cd89e40abf645
ben-s-world <45124610+ben-s-world@users.noreply.github.com>
2019-01-08 12:19:35 +0100

    Preisberechnung testing

---
+++ Test/src/com/testing/AllTests.java
@@ -0,0 +1,11 @@
+package com.testing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ KundeRegistrieren.class })
+public class AllTests {
+
+}
--- Test/src/com/testing/PreisberechnungAusleihe.java
+++ Test/src/com/testing/PreisberechnungAusleihe.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import javax.swing.JCheckBox;
 import javax.swing.JTextField;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import Datenbankmodels.BuchungAnzeigeModel;
@@ -16,16 +17,21 @@ import Steuerung.KundeAnlegenStrg;
 public class PreisberechnungAusleihe {
 
 	
-	
+	@Test
 	public void testBuchungAnzeige() {
 		
 		BuchungAnzeigeModel testBuchungAnzeige = new BuchungAnzeigeModel();
 		int[] tageStunden = new int[2];
-		tageStunden = testBuchungAnzeige.holeTageStunden("2018-12-31 14:34:22", "2019-12-22 14:34:22");
+		tageStunden = testBuchungAnzeige.holeTageStunden("2018-12-31 14:34:22", "2019-12-31 14:34:22");
 		
 		for(int i = 0; i < 2; i++) {
 			System.out.println(tageStunden[i]);
 		}
+		int [] array=new int [2];
+		array [0] = 365;
+		array [1] = 0;
+		Assert.assertArrayEquals(array, tageStunden);
+		
 
 		
 	}
